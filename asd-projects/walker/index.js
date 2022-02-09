aeaa1e311e3e850b065f8b959d3567c4f9e46f08
JonDempsey <jdempopspark@gmail.com>
2022-02-09 00:41:22 +0000

    saving walker

--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -12,11 +12,30 @@ function runProgram(){
   var FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
   
   // Game Item Objects
+var KEY = {
+  "LEFT": 37,
+  "UP": 38,
+  "RIGHT": 39,
+  "DOWN": 40,
+  "W": 87,
+  "A": 65,
+  "S": 83,
+  "D": 68,
+}
+var walkerPositionX = 0;
+var walkerPositionY = 0;
+var walkerSpeedX = 0;
+var walkerSpeedY = 0;
 
+var runnerPositionX = 0;
+var runnerPositionY = 0;
+var runnerSpeedX = 0;
+var runnerSpeedY = 0;
 
   // one-time setup
   var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
-  $(document).on('eventType', handleEvent);                           // change 'eventType' to the type of event you want to handle
+  $(document).on('keydown', handleKeyDown);                           // change 'eventType' to the type of event you want to handle
+  $(document).on('keyup', handleKeyUp);
 
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
@@ -27,21 +46,98 @@ function runProgram(){
   by calling this function and executing the code inside.
   */
   function newFrame() {
+    repositionGameItem();
+    if (walkerPositionX < -15){
+      walkerPositionX = 0;
+    }
+    if (walkerPositionX > 405){
+      walkerPositionX = 390;
+    }
+    if (walkerPositionY < -15){
+      walkerPositionY = 0;
+    }
+    if (walkerPositionY > 405){
+      walkerPositionY = 390;
+    }
     
-
+    if (runnerPositionX < -15){
+      runnerPositionX = 0;
+    }
+    if (runnerPositionX > 405){
+      runnerPositionX = 390;
+    }
+    if (runnerPositionY < -15){
+      runnerPositionY = 0;
+    }
+    if (runnerPositionY > 405){
+      runnerPositionY = 390;
+    }
+    redrawGameItem();
   }
   
   /* 
   Called in response to events.
   */
-  function handleEvent(event) {
+  function handleKeyDown(event) {
+    if (event.which === KEY.LEFT) {
+      walkerSpeedX = -10;
+    }  
+    if (event.which === KEY.UP) {
+      walkerSpeedY = -10;
+    }  
+    if (event.which === KEY.RIGHT) {
+      walkerSpeedX = 10;
+    }  
+    if (event.which === KEY.DOWN) {
+      walkerSpeedY = 10;
+    }  
 
+    if (event.which === KEY.A) {
+      runnerSpeedX = -10;
+    }  
+    if (event.which === KEY.W) {
+      runnerSpeedY = -10;
+    }  
+    if (event.which === KEY.D) {
+      runnerSpeedX = 10;
+    }  
+    if (event.which === KEY.S) {
+      runnerSpeedY = 10;
+    }  
+  }
+  function handleKeyUp(event){
+    if ((event.which === KEY.LEFT) || (event.which === KEY.RIGHT)){
+      walkerSpeedX = 0;
+    }
+    if ((event.which === KEY.UP) || (event.which === KEY.DOWN)){
+      walkerSpeedY = 0;
+    }
+    if ((event.which === KEY.A) || (event.which === KEY.D)){
+      runnerSpeedX = 0;
+    }  
+    if ((event.which === KEY.W) || (event.which ===KEY.S))
+      runnerSpeedY = 0;
   }
 
   ////////////////////////////////////////////////////////////////////////////////
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
 
+  function repositionGameItem() {
+    walkerPositionX += walkerSpeedX;
+    walkerPositionY += walkerSpeedY;
+
+    runnerPositionX += runnerSpeedX;
+    runnerPositionY += runnerSpeedY;
+  }
+
+  function redrawGameItem() {
+    $("#walker").css("left", walkerPositionX);
+    $("#walker").css("top", walkerPositionY);
+
+    $("#runner").css("left", runnerPositionX);
+    $("#runner").css("top", runnerPositionY);
+  }
   
   function endGame() {
     // stop the interval timer
