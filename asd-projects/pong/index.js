0a8f435b36e0579daba91e4db4b0525872c8d3f0
JonDempsey <jdempopspark@gmail.com>
2022-04-20 00:22:06 +0000

    paddle key responses complete

--- asd-projects/pong/index.js
+++ asd-projects/pong/index.js
@@ -13,10 +13,30 @@ function runProgram(){
   
   // Game Item Objects
 
+  var KEY = {
+    W: 87,
+    S: 83,
+    UP: 38,
+    DOWN: 40,
+  }
+
+  function createGameItem(id, speedX, speedY){
+    let obj = {}
+    obj.id = id;
+    obj.x = parseFloat($(obj.id).css("left"));
+    obj.y = parseFloat($(obj.id).css("top"));
+    obj.speedX = speedX;
+    obj.speedY = speedY;
+    return obj;
+  }
+  
+  var player1 = createGameItem("#paddle1", 0, 20);
+  var player2 = createGameItem("#paddle2", 0, 20);
 
   // one-time setup
   let interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
-  $(document).on('eventType', handleEvent);                           // change 'eventType' to the type of event you want to handle
+  $(document).on('keydown', handleKeyDown);                           // change 'eventType' to the type of event you want to handle
+  $(document).on('keyup', handleKeyUp);
 
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
@@ -27,20 +47,86 @@ function runProgram(){
   by calling this function and executing the code inside.
   */
   function newFrame() {
-    
+    repositionPaddles();
+    paddleBoundaries();
+    redrawPaddles();
+
 
   }
   
   /* 
   Called in response to events.
   */
-  function handleEvent(event) {
+  function handleKeyDown(event) {
+    var keycode = event.which;
+    // player 1 controls
+    if (keycode === KEY.W) {
+      player1.speedY = -10;
+    }
+    if (keycode === KEY.S) {
+      player1.speedY = 10;
+    }
+    // player 2 controls
+    if (keycode === KEY.UP) {
+      player2.speedY = -10
+    }
+    if (keycode === KEY.DOWN) {
+      player2.speedY = 10
+    }
+  }
 
+  function handleKeyUp(event){
+    var keycode = event.which;
+
+    if (keycode === KEY.W | keycode === KEY.S){
+      player1.speedY = 0;
+    }
+    if (keycode === KEY.UP | keycode === KEY.DOWN){
+      player2.speedY = 0;
+    }
   }
 
   ////////////////////////////////////////////////////////////////////////////////
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
+
+  function repositionPaddles(){
+    player1.y += player1.speedY;
+    player2.y += player2.speedY;
+  }
+
+  function paddleBoundaries(){
+    if (player1.y < 0){
+      player1.y = 0;
+    }
+    if (player1.y > 360){
+      player1.y = 360;
+    }
+    if (player2.y < 0){
+      player2.y = 0;
+    }
+    if (player2.y > 360){
+      player2.y = 360;
+    }
+  }
+
+  function redrawPaddles(){
+    $(player1.id).css("top", player1.y);
+    $(player2.id).css("top", player2.y);
+  }
+
+  /* function movePaddleUp(player){
+    if (player.y > 0) {
+      player.y -= player.speedY;
+      $(player.id).css("top", player.y);
+    }
+  }
+  function movePaddleDown(player){
+    if (player.y < 360) {
+      player.y += player.speedY;
+      $(player.id).css("top", player.y);
+    }
+  } */
 
   
   function endGame() {
