196adb72597e1024e1b77c5d075b4372807f9c16
JonDempsey <jdempopspark@gmail.com>
2023-03-29 00:57:47 +0000

    modified:   video-game-course/create-js/index.html 	modified:   video-game-course/create-js/index.js

--- video-game-course/create-js/index.js
+++ video-game-course/create-js/index.js
@@ -4,10 +4,11 @@
  */
 (function(window, createjs) {
   // TODO 5: Initialize CreateJS //
-  
+  const canvas = document.getElementById("canvas");
+  const stage = new createjs.Stage(canvas);
 
   // TODO 6: Set the framerate of the Ticker
-  
+  createjs.Ticker.framerate = 12;
 
 
   /*
@@ -22,7 +23,7 @@
     circle = new createjs.Shape();
     
   // CREATE A BACKGROUND //
-  bg.graphics.beginFill('#CCC').drawRect(0, 0, canvas.width, canvas.height);
+  bg.graphics.beginFill('#A00').drawRect(0, 0, canvas.width, canvas.height);
     
   // CREATE A CIRCLE //
   circle.graphics.beginFill('blue').drawCircle(0, 0, radius);
@@ -31,21 +32,32 @@
 
   // ADD DISPLAY OBJECTS TO STAGE //
   stage.addChild(bg, circle);
+  stage.update()
 
 
   // TODO 8: Listen to the 'tick' event  //
-  
+  let tickHandler = createjs.Ticker.on('tick', onTick);
   
 
   // TODO 9: Handle the 'tick' event //
+  function onTick(event){
+    update(event);
+  }
   
-  
-
   /*
    * TODO 10: Implement an update Function, after making 
    * changes to assets, it must call stage.update(); 
    */
-  
-  
+
+  function update(event){
+    //circle.clear();
+    //circle.setTransform(circle.x, circle.y, 1.5);
+    //circle.moveTo(Math.random()*canvas.width, Math.random()*canvas.height);
+    //circle.updateCache();
+    circle.x = Math.random() * canvas.width;
+    circle.y = Math.random() * canvas.height;
+
+    stage.update();
+  }
 
 }(window, window.createjs));
